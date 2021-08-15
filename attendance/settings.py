74f64da2ff6131256fb498355d7ee5d04f8cca96
Piyush20001 <44575274+Piyush20001@users.noreply.github.com>
2021-08-15 23:09:01 +0530

    fix

--- attendance/settings.py
+++ attendance/settings.py
@@ -132,4 +132,3 @@ MEDIA_ROOT = BASE_DIR/'files'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(locals())
